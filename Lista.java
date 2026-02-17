34ae5508ba97a924d5753a57d455b9aa534c630d
FNC-linoMaselli <andrea.maselli@marconibari.edu.it>
2026-02-17 11:08:02 +0000

    consegna

--- Lista.java
+++ Lista.java
@@ -1,1 +1,49 @@
-//Implementa qui la classe Lista
+public class Lista<T> {
+
+    Nodo<T> head;
+    
+    public Lista<T>(Nodo<T> head) {
+        head = null;
+    }
+
+    public boolean vuota() {
+        if (head.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void aggiungiInTesta(T dato) {
+        Nodo<T> nuovoNodo = new Nodo(dato);
+        nuovoNodo.next = head;
+        head = nuovoNodo;
+    }
+
+    public void aggiungiInCoda(T dato) {
+        Nodo <T> nuovoNodo = new Nodo(dato);
+
+        if (head == null) {
+            head = nuovoNodo;
+        }
+
+        Nodo<T> curr = head;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        curr.next = nuovoNodo;
+    }
+
+    public void aggiungiInPosizione(T dato, int posizione) {
+        Nodo <T> nuovoNodo = new Nodo<>(dato);
+        Nodo<T> curr = head;
+        currI = 0;
+        while (currI<posizione-1) {
+            curr = curr.next;
+            ++currI;
+        }
+        nuovoNodo.next = curr.next;
+        curr.next = nuovoNodo;
+    }
+
+}
--- Nodo.java
+++ Nodo.java
@@ -1,1 +1,27 @@
-//Implementa qui la classe Nodo
+public Class Nodo <T>{
+
+    private T dato;
+    private Nodo next;
+
+    public Nodo <T> (T dato) {
+        this.dato = dato;
+        next = null;
+    }
+
+    public void setDato(T dato) {
+        this.dato = dato;
+    }
+
+    public T getDato() {
+        return dato;
+    }
+
+    public void setNext(Nodo next) {
+        this.next = next;
+    }
+
+    public Nodo getNext() {
+        return next;
+    }
+
+}
